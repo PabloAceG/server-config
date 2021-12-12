c63016ec2ded67d662e415281a1d329f358bea37
PabloAceG <pabloacereda98@gmail.com>
2021-12-12 14:11:00 +0100

    Include statistics for disks, vms and format message

--- src/dashboard/notifications/sources/startup-notification.sh
+++ src/dashboard/notifications/sources/startup-notification.sh
@@ -3,15 +3,27 @@
 # Authentication
 CHAT_ID=""
 TOKEN=""
-URL="https://api.telegram.org/bot${TOKEN}/sendMessage"
-# Message
-DOCKERPS=$(docker ps --all --format "table {{.Names}}\t{{.State}}")
-DOCKERMSG="\n <b><i>Running containers:</i></b><pre>$DOCKERPS</pre>"
-IPADDRESS=$(curl ifconfig.me)
-IPADDRESSMSG="\n <b><i>Current IP Address</i></b>:\n $IPADDRESS\n"
-MESSAGE="$DOCKERMSG $IPADDRESSMSG"
+MSGAPI="https://api.telegram.org/bot${TOKEN}/sendMessage"
 
-curl -s -X POST "${URL}" \
+# Message
+NODENAME=$(cat /etc/hostname)
+NODENAMEMSG="<b><u>Node Name</u></b>\n<pre>${NODENAME}</pre>\n"
+
+IPADDRESS=$(curl ifconfig.me)
+IPADDRESSMSG="\n<b><i>Current IP Address</i></b>\n${IPADDRESS}\n"
+
+DOCKERPS=$(docker ps --all --format "table {{.Names}}\t{{.State}}")
+DOCKERMSG="\n<b><i>Docker Containers Status</i></b>\n<pre>${DOCKERPS}</pre>\n"
+
+VMLIST=$(qm list | awk '{ $4=$5=$6=""; print $0 }' | column -t)
+VMLISTMSG="\n<b><i>VMs Status</i></b>\n<pre>${VMLIST}</pre>\n"
+
+DISKUSG=$(df -h -x squashfs -x tmpfs -x devtmpfs --output=source,size,pcent,avail)
+DISKUSGMSG="\n<b><i>Disk Statistics</i></b>\n<pre>${DISKUSG}</pre>\n"
+
+MESSAGE="${NODENAMEMSG} ${IPADDRESSMSG} ${DOCKERMSG} ${VMLISTMSG} ${DISKUSGMSG}"
+
+curl -s -X POST "${MSGAPI}" \
     -d chat_id="${CHAT_ID}" \
-    -d text="$(echo -e "$MESSAGE")"  \
+    -d text="$(echo -e "$MESSAGE")" \
     -d parse_mode=html
